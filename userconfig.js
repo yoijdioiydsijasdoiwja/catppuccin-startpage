d867f42a5ee642bfd23fe86c9a348b8d148162fa
yoijdioiydsijasdoiwja <knockfxx@gmail.com>
2025-01-15 19:08:20 +0100

    Revert "Update userconfig.js"
    This reverts commit 18e8b2fccc67638c253fe4692b929b2b43efe090.

--- userconfig.js
+++ userconfig.js
@@ -1,10 +1,10 @@
 // latte / frappe / macchiato / mocha
-const palette = mocha;
+const palette = macchiato;
 
 const default_config = {
   overrideStorage: true,
   temperature: {
-    location: "Chemnitz",
+    location: "London",
     scale: "C",
   },
   clock: {
@@ -65,6 +65,43 @@ const default_config = {
             },
           ],
         },
+        {
+          name: "media",
+          links: [
+            {
+              name: "уп",
+              url: "https://www.pravda.com.ua",
+              icon: "news",
+              icon_color: palette.green,
+            },
+            {
+              name: "mil.in.ua",
+              url: "https://mil.in.ua",
+              icon: "badge-filled",
+              icon_color: palette.peach,
+            },
+            {
+              name: "куток",
+              url: "https://kutok.io",
+              icon: "border-radius",
+              icon_color: palette.red,
+            },
+            {
+              name: "ґрунт",
+              url: "https://grnt.media",
+              icon: "eye-bolt",
+              icon_color: palette.blue,
+            },
+            {
+              name: "village",
+              url: "https://www.village.com.ua",
+              icon: "home-2",
+              icon_color: palette.mauve,
+            },
+          ],
+        },
+      ],
+    },
     {
       name: "dev",
       background_url: "src/img/banners/cbg-8.gif",
